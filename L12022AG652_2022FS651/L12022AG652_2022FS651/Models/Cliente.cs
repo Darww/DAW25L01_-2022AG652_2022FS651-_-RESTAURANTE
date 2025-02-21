6463324f58a10ae65f9a5bf5a55595b2f1af4c35
DARWIN PROYECTOS <darwin.aguirre1@catolica.edu.sv>
2025-02-20 18:27:25 -0600

    creacion de clases

---
+++ L12022AG652_2022FS651/L12022AG652_2022FS651/Models/Cliente.cs
@@ -0,0 +1,36 @@
+﻿namespace L12022AG652_2022FS651.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string NombreCliente { get; set; }
+        public string Direccion { get; set; }
+    }
+
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+        public int MotoristaId { get; set; }
+        public int ClienteId { get; set; }
+        public int PlatoId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+    }
+
+    public class Plato
+    {
+        public int PlatoId { get; set; }
+        public string NombrePlato { get; set; }
+        public decimal Precio { get; set; }
+    }
+
+    public class Motorista
+    {
+        public int MotoristaId { get; set; }
+        public string NombreMotorista { get; set; }
+    }
+
+
+
+}
+
