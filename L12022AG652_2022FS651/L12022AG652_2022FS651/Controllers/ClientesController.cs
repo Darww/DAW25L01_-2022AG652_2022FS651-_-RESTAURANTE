3914c1273a16e71edebe7ac4d42326c3bad8cf49
Gabriela <gabriela.flores7@catolica.edu.sv>
2025-02-20 19:47:43 -0600

    Controladores

---
+++ L12022AG652_2022FS651/L12022AG652_2022FS651/Controllers/ClientesController.cs
@@ -0,0 +1,16 @@
+﻿using L12022AG652_2022FS651.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L12022AG652_2022FS651.Controllers
+{
+    public class ClientesController
+    {
+        [HttpGet("filtrar/{direccion}")]
+        public async Task<ActionResult<IEnumerable<Cliente>>> FiltrarPorDireccion(string direccion)
+        {
+            return await _context.Clientes.Where(c => c.Direccion.Contains(direccion)).ToListAsync();
+        }
+
+    }
+}
---
+++ L12022AG652_2022FS651/L12022AG652_2022FS651/Controllers/PlatosController.cs
@@ -0,0 +1,25 @@
+﻿using L12022AG652_2022FS651.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace L12022AG652_2022FS651.Controllers
+{
+    public class PlatosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+
+        [HttpGet("buscar/{nombre}")]
+        public async Task<ActionResult<IEnumerable<Plato>>> BuscarPorNombre(string nombre)
+        {
+            return await _context.Platos.Where(p => p.NombrePlato.Contains(nombre)).ToListAsync();
+        }
+
+    }
+
+
+}
